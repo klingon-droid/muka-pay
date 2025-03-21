d998ce8b3b6692ea5cd565b97a7736897bcf1d4f
yongfeng <yongfeng99@gmail.com>
2025-03-22 03:35:01 +0800

    deposit transaction

--- src/components/Deposit.vue
+++ src/components/Deposit.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full h-full p-4 space-y-4 pb-[25vh] overflow-y-auto">
-        <div v-if="!username" class="w-full p-8 text-center space-y-4">
+        <div v-if="!currentUsername" class="w-full p-8 text-center space-y-4">
             <p class="text-xl font-bold text-red-500">Invalid deposit link</p>
             <p class="text-gray-600">Please use a valid deposit link with a username parameter</p>
         </div>
@@ -8,16 +8,14 @@
         <div v-else class="w-full p-8 text-center space-y-4">
             <div class="mb-12">
                 <h1 class="text-2xl font-medium text-gray-600 mb-2">Deposit Base USDC to</h1>
-                <p class="text-4xl font-bold">@{{ username }}</p>
+                <p class="text-4xl font-bold">@{{ currentUsername }}</p>
             </div>
 
             <div class="flex gap-2 max-w-4xl mx-auto">
-                <button class="flex-1 bg-white border border-gray-200 hover:border-gray-300 transition-colors text-gray-800 py-3 px-4 rounded-lg flex items-center justify-center gap-2 text-sm font-medium">
+                <button @click="copyLink" class="flex-1 bg-white border border-gray-200 hover:border-gray-300 transition-colors text-gray-800 py-3 px-4 rounded-lg flex items-center justify-center gap-2 text-sm font-medium">
                     <iconify-icon icon="mdi:content-copy" class="text-lg"></iconify-icon>
                     Copy link
                 </button>
-                
-                
             </div>
         </div>
 
@@ -25,7 +23,7 @@
 
             <p class="text-center mb-8">Connect Wallet to Base</p>
 
-            <div class="w-full flex justify-center items-center bg-white p-4 rounded-xl mb-4">
+            <div v-if="!isConnected" class="w-full flex justify-center items-center bg-white p-4 rounded-xl mb-4">
                 <p>Address: Not Connected</p>
             </div>
 
@@ -33,7 +31,7 @@
                 <button v-if="!isConnected" @click="connectWallet" class="w-full bg-black text-xl text-white p-4 rounded-xl">Connect</button>
                 <button v-if="isConnected && !isBaseNetwork" @click="switchToBase" class="w-full bg-black text-xl text-white p-4 rounded-xl">Switch to Base</button>
 
-                <button v-if="isConnected && isBaseNetwork" @click="disconnect" class="w-full bg-red-500 text-xl text-white p-4 rounded-xl">Disconnect</button>
+                <button v-if="isConnected && isBaseNetwork" @click="disconnectWallet" class="w-full bg-red-500 text-xl text-white p-4 rounded-xl">Disconnect</button>
             </div>
 
         </div>
@@ -43,6 +41,7 @@
 
             <div class="w-full flex justify-center items-center gap-2 bg-white p-4 rounded-xl mb-4">
                 <input 
+                    v-model="inputAmount"
                     type="number" 
                     class="flex-1 "
                     placeholder="Enter USDC amount"
@@ -77,37 +76,151 @@
 
 <script setup>
 
-    import { ref, onMounted } from 'vue';
+    import { ref, onMounted, computed } from 'vue';
 
     const isConnected = ref(false);
-    const isBaseNetwork = ref(false);
+    // const isBaseNetwork = ref(false);
     const isAmountConfirmed = ref(false);
     const isApprovalSufficient = ref(false);
-    const username = ref('');
+    const currentUsername = ref('');
+    const inputAmount = ref(0);
+
+    import { wagmiConfig, walletAccount, ERC20_ABI, VAULT_CONTRACT_ADDRESS, USDC_CONTRACT_ADDRESS, getUsernameHash, VAULT_ABI } from '../stores/user'
+    import { useStore } from '@nanostores/vue'
+    import { getAccount, getConnectors, connect, disconnect, switchChain, reconnect, writeContract, waitForTransactionReceipt, readContract } from '@wagmi/core'
+    import { baseSepolia } from 'wagmi/chains'
+    import { formatUnits, parseUnits } from 'viem'
+
 
     onMounted(() => {
         const urlParams = new URLSearchParams(window.location.search);
-        username.value = urlParams.get('username') || '';
+        currentUsername.value = urlParams.get('username') || '';
+        reconnect(wagmiConfig);
+
+        const account = getAccount(wagmiConfig)
+        if(account) {
+            console.log('account:', account)
+            walletAccount.set(account);
+            isConnected.value = account.isConnected;
+        }
+
     });
 
-    const connectWallet = () => {
-        isConnected.value = true;
+    const connectWallet = async () => {
+        const _connectors = getConnectors(wagmiConfig)
+        await connect(wagmiConfig, { connector: _connectors[0] })
+        const account = getAccount(wagmiConfig)
+        if(account) {
+            console.log('account:', account)
+            walletAccount.set(account);
+            isConnected.value = account.isConnected;
+        }
+    }
+
+    const disconnectWallet = () => {
+        disconnect(wagmiConfig)
+        isConnected.value = false;
+        walletAccount.set(null);
     }
 
     const switchToBase = () => {
-        isBaseNetwork.value = true;
+        switchChain(wagmiConfig, { chainId: baseSepolia.id })
+        const account = getAccount(wagmiConfig)
+        if(account) {
+            console.log('account:', account)
+            walletAccount.set(account);
+            isConnected.value = account.isConnected;
+        }
     }
+
+    const isBaseNetwork = computed(() => {
+        return walletAccount?.value?.chainId === baseSepolia.id;
+    })
 
     const confirmAmount = () => {
         isAmountConfirmed.value = true;
     }
 
-    const handleApproval = () => {
-        isApprovalSufficient.value = true;
+    const checkApproval = async () => {
+        const allowance = await readContract(wagmiConfig, {
+            address: USDC_CONTRACT_ADDRESS,
+            abi: ERC20_ABI,
+            functionName: 'allowance',
+            args: [
+                walletAccount.value.address,
+                VAULT_CONTRACT_ADDRESS
+            ]
+        })
+        console.log('allowance:', allowance)
+        return Number(formatUnits(allowance, 6)) >= Number(inputAmount.value);
     }
 
-    const handleDeposit = () => {
-        alert('Deposit');
+    const handleApproval = async () => {
+
+        const allowed = await checkApproval();
+        if(allowed) {
+            isApprovalSufficient.value = true;
+            return;
+        }
+
+        try {
+
+            const tx = await writeContract(wagmiConfig, {
+                address: USDC_CONTRACT_ADDRESS,
+                abi: ERC20_ABI,
+                functionName: 'approve',
+                args: [
+                    VAULT_CONTRACT_ADDRESS, 
+                    String(Number(inputAmount.value) * 1e6)
+                ]
+            })
+
+            const receipt = await waitForTransactionReceipt(wagmiConfig, {
+                hash: tx
+            })
+
+            console.log('receipt:', receipt)
+            isApprovalSufficient.value = receipt.status === 'success';
+
+        } catch (error) {
+            console.error('Error approving:', error)
+        }
+    }
+
+    const handleDeposit = async () => {
+        
+        try {
+            const usernameHash = await getUsernameHash(currentUsername.value);
+            console.log('usernameHash:', usernameHash)
+
+            const tx = await writeContract(wagmiConfig, {
+                address: VAULT_CONTRACT_ADDRESS,
+                abi: VAULT_ABI,
+                functionName: 'deposit',
+                args: [
+                    usernameHash, 
+                    String(Number(inputAmount.value) * 1e6)
+                ]
+            })
+
+            const receipt = await waitForTransactionReceipt(wagmiConfig, {
+                hash: tx
+            })
+
+            console.log('receipt:', receipt)
+            // isDepositSufficient.value = receipt.status === 'success';
+            alert('Deposit successful');
+
+        } catch (error) {
+            console.error('Error depositing:', error)
+        }
+    }
+
+
+    const copyLink = () => {
+        const link = window.location.href;
+        navigator.clipboard.writeText(link);
+        alert('Link copied to clipboard');
     }
 
 
--- src/stores/user.js
+++ src/stores/user.js
@@ -7,30 +7,31 @@ export function setUsername(name) {
     username.set(name)
 }
 
+import * as circomlibjs from 'circomlibjs'
 
-// // Convert string to field element
-// function strToField(str) {
-//     const bytes = Buffer.from(str);
-//     let result = 0n;
-//     for (let i = 0; i < bytes.length; i++) {
-//         result = (result << 8n) + BigInt(bytes[i]);
-//     }
-//     return result;
-// }
+// Convert string to field element
+function strToField(str) {
+    const bytes = Buffer.from(str);
+    let result = 0n;
+    for (let i = 0; i < bytes.length; i++) {
+        result = (result << 8n) + BigInt(bytes[i]);
+    }
+    return result;
+}
 
-// export async function getUsernameHash() {
-//     // Initialize Poseidon
-//     const poseidon = await circomlibjs.buildPoseidon();
+export async function getUsernameHash(_username) {
+    // Initialize Poseidon
+    const poseidon = await circomlibjs.buildPoseidon();
 
-//     // Convert username to field element
-//     const usernameField = strToField(username);
+    // Convert username to field element
+    const usernameField = strToField(_username);
 
-//     // Generate Poseidon hash
-//     const usernameHash = poseidon([usernameField]);
+    // Generate Poseidon hash
+    const usernameHash = poseidon([usernameField]);
 
-//     // Return the hash as a string
-//     return poseidon.F.toString(usernameHash);
-// }
+    // Return the hash as a string
+    return poseidon.F.toString(usernameHash);
+}
 
 
 
@@ -41,6 +42,8 @@ import { createConfig, http } from 'wagmi'
 import { baseSepolia } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 
+export const walletAccount = atom(null)
+
 export const wagmiConfig = createConfig({
   chains: [baseSepolia],
   transports: {
@@ -50,3 +53,80 @@ export const wagmiConfig = createConfig({
     injected()
   ]
 })
+
+
+export const VAULT_CONTRACT_ADDRESS = "0xB4f7dF8d5ec4C61fF41040230fCF23d167220741";
+export const USDC_CONTRACT_ADDRESS = "0x0a6CC1B2cB197AA6a6878fee28Fd1c908B603ad4";
+
+
+export const ERC20_ABI = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
+
+export const VAULT_ABI = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "usernameHash",
+                "type": "uint256"
+            },
+            {
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "deposit",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+]
+
+
