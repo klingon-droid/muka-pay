5100409d062add6d7c88799c8fda21f37a713c7f
yongfeng <yongfeng99@gmail.com>
2025-03-22 02:30:49 +0800

    wallet

--- src/stores/user.js
+++ src/stores/user.js
@@ -5,4 +5,48 @@ export const username = atom('')
 export function setUsername(name) {
     console.log('setting username:', name)
     username.set(name)
-} 
+}
+
+
+// // Convert string to field element
+// function strToField(str) {
+//     const bytes = Buffer.from(str);
+//     let result = 0n;
+//     for (let i = 0; i < bytes.length; i++) {
+//         result = (result << 8n) + BigInt(bytes[i]);
+//     }
+//     return result;
+// }
+
+// export async function getUsernameHash() {
+//     // Initialize Poseidon
+//     const poseidon = await circomlibjs.buildPoseidon();
+
+//     // Convert username to field element
+//     const usernameField = strToField(username);
+
+//     // Generate Poseidon hash
+//     const usernameHash = poseidon([usernameField]);
+
+//     // Return the hash as a string
+//     return poseidon.F.toString(usernameHash);
+// }
+
+
+
+
+
+
+import { createConfig, http } from 'wagmi'
+import { baseSepolia } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
+
+export const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http()
+  },
+  connectors: [
+    injected()
+  ]
+})
